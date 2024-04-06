8d7847e7caed1b2dafba59fa11d736d528e247a1
ramirocanario1 <ramirocanario1@gmail.com>
2024-04-06 15:56:03 -0300

    feat(2): generar con distribucion poisson

--- punto2.py
+++ punto2.py
@@ -1,8 +1,7 @@
 from time import sleep
-from random import expovariate
-from random import gauss
-from math import log
+from random import expovariate, gauss, random
 from matplotlib import pyplot as plt
+from math import exp
 
 
 def generar_exponencial(cantidad=100, beta=1):
@@ -21,6 +20,22 @@ def generar_gauss(cantidad=100, media=0, desvio=1):
     plt.show()
 
 
+def generar_poisson(cantidad=100, l=1):
+    valores = []
+    for _ in range(cantidad):
+        i = 0
+        p = 1
+        while p >= exp(-l):
+            p *= random()
+            i += 1
+        valores.append(i)
+    promedio = sum(valores) / len(valores)
+    print(f"El promedio es: {promedio}")
+    plt.hist(valores, bins=50, density=True)
+    plt.show()
+
+
 if __name__ == "__main__":
     generar_exponencial(1000, beta=0.75)
     generar_gauss(1000, media=10, desvio=1)
+    generar_poisson(1000, l=1)
