ab072e550b61292adc619556fde672ff8f1fd51e
ramirocanario1 <ramirocanario1@gmail.com>
2024-04-06 15:57:13 -0300

    feat(2): generar con distribucion uniforme

--- punto2.py
+++ punto2.py
@@ -35,7 +35,16 @@ def generar_poisson(cantidad=100, l=1):
     plt.show()
 
 
+def generar_uniforme(cantidad=100, a=0, b=1):
+    valores = [random() * (b - a) + a for _ in range(cantidad)]
+    promedio = sum(valores) / len(valores)
+    print(f"El promedio es: {promedio}")
+    plt.hist(valores, bins=50, density=True)
+    plt.show()
+
+
 if __name__ == "__main__":
     generar_exponencial(1000, beta=0.75)
     generar_gauss(1000, media=10, desvio=1)
     generar_poisson(1000, l=1)
+    generar_uniforme(1000, a=0, b=1)
