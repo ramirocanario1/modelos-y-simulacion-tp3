9e5b242d71d0d65e38bc25e6506666d182cfa7a4
ramirocanario1 <ramirocanario1@gmail.com>
2024-04-06 15:43:29 -0300

    feat(2): generar exponencial y gauss con parametros personalizados

--- punto2.py
+++ punto2.py
@@ -5,23 +5,22 @@ from math import log
 from matplotlib import pyplot as plt
 
 
-def generar_exponencial(cantidad):
-    valores = [expovariate(1) for _ in range(cantidad)]
-    media = sum(valores) / len(valores)
-    print(f"El promedio es: {media}")
+def generar_exponencial(cantidad=100, beta=1):
+    valores = [expovariate(beta) for _ in range(cantidad)]
+    promedio = sum(valores) / len(valores)
+    print(f"El promedio es: {promedio}")
     plt.hist(valores, bins=50, density=True)
     plt.show()
 
 
-def generar_gauss(cantidad):
-    valores = [gauss(0, 1) for _ in range(cantidad)]
-    media = sum(valores) / len(valores)
-    print(f"El promedio es: {media}")
+def generar_gauss(cantidad=100, media=0, desvio=1):
+    valores = [gauss(media, desvio) for _ in range(cantidad)]
+    promedio = sum(valores) / len(valores)
+    print(f"El promedio es: {promedio}")
     plt.hist(valores, bins=50, density=True)
     plt.show()
 
 
 if __name__ == "__main__":
-    # Generar 1000 valores aleatorios con distribucion exponencial
-    generar_exponencial(1000)
-    generar_gauss(1000)
+    generar_exponencial(1000, beta=0.75)
+    generar_gauss(1000, media=10, desvio=1)
